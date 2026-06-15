c6ac716992e665fcf5b1fa088a4e8ae2146b8f60
mubashir111 <muba4shir@gmail.com>
2026-06-15 15:52:44 +0530

    updated the mail funtions

---
+++ php/phpmailer/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ======================================
+ * HTWF - SMTP CONFIG FILE
+ * ======================================
+ *
+ * From this file you can set SMTP informations.
+ * To enable the SMTP on your contact form add the attribute data-engine="smtp" to the form object.
+ * Documentation at html.framework-y.com/components/contact-form/
+ */
+
+$smtp_config = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'hydrotechdubai@gmail.com',
+    'password' => 'exmb tyma pftv enab',
+    'port' => '465',
+    'email_from' => 'hydrotechdubai@gmail.com'
+);
+
+
+
+?>
